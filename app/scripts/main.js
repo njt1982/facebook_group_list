bb37625f8dff295cbb163ed3464c5bd62cfd86b4
Nicholas Thompson <njt1982@gmail.com>
2015-07-12 01:30:31 +0100

    Major work. Caching. Fixed search.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,23 +16,24 @@ require.config({
     underscore: '../bower_components/lodash/dist/lodash',
     hello: '../bower_components/hello/dist/hello.all.min',
     bootstrap: '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap',
-    'backbone-fetch-cache': '../bower_components/backbone-fetch-cache/backbone.fetch-cache',
-    'bootstrap-sass-official': '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap',
+    backboneFetchCache: '../bower_components/backbone-fetch-cache/backbone.fetch-cache',
+    bootstrapSassOfficial: '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap',
     lodash: '../bower_components/lodash/dist/lodash.compat',
     modernizr: '../bower_components/modernizr/modernizr',
     requirejs: '../bower_components/requirejs/require',
-    'requirejs-text': '../bower_components/requirejs-text/text'
+    requirejsText: '../bower_components/requirejs-text/text',
+    listjs : '../bower_components/listjs/dist/list'
   },
-  packages: [
-
-  ]
+  packages: []
 });
 
+
+
 require([
-  'backbone', 'hello', 'jquery', 'underscore'
-], function (Backbone, Hello, $, _) {
+  'backbone', 'hello', 'jquery', 'underscore', 'listjs', 'backboneFetchCache'
+], function (Backbone, Hello, $, _, List) {
   var $loginButton = $('#facebook_login'),
-      $logoutMenu = $('#logout_menu');
+    $logoutMenu = $('#logout_menu');
 
   Hello.init({
     facebook: '483331355160349'
@@ -44,93 +45,178 @@ require([
   var facebook = Hello('facebook');
 
 
+  var GroupFeedItem = Backbone.Model.extend({});
 
-  var Group = Backbone.Model.extend({
-    name: function() {
-      console.log(this);
-      return this.attributes.name;
+  var GroupFeedItemCollection = Backbone.Collection.extend({
+    model: GroupFeedItem,
+    groupId: null,
+
+    initialize: function (groupId) {
+      this.groupId = groupId;
+      this.fetch({ cache: true, url:  this.groupId + '/feed?limit=200&fields=id,from,message'});
     },
-    html_url: function() {
-      ;
-    }
-  });
-
-  var GroupCollection = Backbone.Collection.extend({
-    model: Group,
     sync: function (method, collection, options) {
       if (method == 'read') {
-        facebook.api('me/groups').then(function (response) {
+        var d = $.Deferred();
+
+        facebook.api(options.url).then(function (response) {
+          d.resolve(response.data);
           options.success(response.data);
+        }, function(response) {
+          d.fail(response);
         });
+
+        return d;
       }
     }
   });
-  var groups = new GroupCollection;
+
+
+  var Group = Backbone.Model.extend({});
+
+  var GroupCollection = Backbone.Collection.extend({
+    model: Group,
+    initialize: function () {
+      this.fetch({ cache: true, url: 'me/groups' });
+    },
+    sync: function (method, collection, options) {
+      if (method == 'read') {
+        var d = $.Deferred();
+
+        facebook.api(options.url).then(function (response) {
+          d.resolve(response.data);
+          options.success(response.data);
+        }, function(response) {
+          d.fail(response);
+        });
+
+        return d;
+      }
+    }
+  });
 
 
   var GroupView = Backbone.View.extend({
     tagName: 'div',
+    className: 'group col-sm-4 col-md-4',
 
     template: _.template($('#group-template').html()),
 
     initialize: function () {
       this.listenTo(this.model, 'change', this.render);
     },
+    events: {
+      "click button.group-search": "navigateToGroup"
+    },
+    navigateToGroup: function () {
+      Backbone.history.navigate('group/' + this.model.id, {trigger: true});
+    },
 
     render: function () {
-      var data = _.extend(this.model.toJSON(), {
-        getUrl: function() { return 'https://www.facebook.com/groups/' + this.id; }
-      });
-      this.$el.html(this.template(data));
+      this.$el.html(this.template(this.model.toJSON()));
       return this;
     }
   });
 
   var GroupsView = Backbone.View.extend({
+    className: 'group-wrapper row',
+    groups: null,
+
     initialize: function () {
-      this.listenTo(groups, 'add', this.addOne);
-      this.listenTo(groups, 'reset', this.addAll);
-      this.listenTo(groups, 'all', this.render);
-      groups.fetch();
+      this.groups = new GroupCollection();
+      this.listenTo(this.groups, 'add', this.addOne);
+      this.listenTo(this.groups, 'reset', this.addAll);
+      this.listenTo(this.groups, 'all', this.render);
     },
 
     addOne: function (group) {
-      var view = new GroupsView({model: group});
+      var view = new GroupView({model: group});
       this.$el.append(view.render().el);
     },
 
     addAll: function () {
-      groups.each(this.addOne, this);
+      this.groups.each(this.addOne, this);
+      this.render();
     }
   });
 
+
+
+  var GroupFeedItemCollectionView = Backbone.View.extend({
+    className: 'item-feed-view row',
+    items: null,
+    list: null,
+
+    initialize: function (groupId) {
+      this.items = new GroupFeedItemCollection(groupId);
+
+      this.$el.append('<input type="text" class="form-control search" placeholder="Search">');
+      this.$el.append('<div class="list col-md-12"></div>');
+
+      this.list = new List(this.el, {
+        valueNames: [ "name", "message"],
+        item: '<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title name"></h4></div><div class="panel-body"><p class="message"></p></div></div>'
+      }, []);
+      this.list.clear();
+
+      this.listenTo(this.items, 'add', this.addOne);
+      this.listenTo(this.items, 'reset', this.addAll);
+      this.listenTo(this.items, 'all', this.render);
+    },
+
+    addOne: function (item) {
+      this.list.add({
+        name: item.attributes.from.name,
+        message: item.attributes.message
+      });
+    },
+
+    addAll: function () {
+      this.items.each(this.addOne, this);
+      this.render();
+    },
+
+  });
+
+
+
+
   var AppView = Backbone.View.extend({
     el: $('#groups'),
+    title: null,
+    links: [],
     childView: null,
 
-    render: function() {
-      this.$el.html("<h2>Welcome</h2>");
+    template: _.template($('#navbar-template').html()),
+
+    render: function () {
+      this.$el.html(this.template({title: this.title, links: this.links }));
       this.$el.append(this.childView.$el);
+      return this;
     }
   });
 
 
   var AppRouter = Backbone.Router.extend({
-    selectedGroup: null,
     appView: null,
 
-    initialize: function() {
-      console.log(this);
-      this.selectedGroup = null;
+    initialize: function () {
       this.appView = new AppView();
     },
     routes: {
       "": "groupListing",
-      "group/:groupId" : "groupPage"
+      "group/:groupId": "groupPage"
     },
-    groupListing: function() {
-      console.log(this);
+    groupListing: function () {
       this.appView.childView = new GroupsView();
+      this.appView.title = 'Browse';
+      this.appView.links = [];
+      this.appView.render();
+    },
+    groupPage: function (id) {
+      this.appView.childView = new GroupFeedItemCollectionView(id);
+      this.appView.title = 'Search';
+      this.appView.links = [{path: '', text: 'Browse'}];
       this.appView.render();
     }
   });
@@ -147,12 +233,13 @@ require([
   else {
     new AppRouter();
   }
+  Backbone.history.start();
 
   facebook.on('auth.login', function (data) {
     if (data.network === 'facebook' && data.authResponse.access_token !== undefined) {
       $loginButton.removeClass('hidden');
+      new AppRouter();
     }
   });
-  Backbone.history.start();
 
 });
